c77cbe910696892096d454e7a92d9c0e4cc5ec0e
Rahul Raval <rahulraval870@gmail.com>
2026-09-16 18:13:47 +0000

    Updated updateK8sManifests.sh

--- scripts/updateK8sManifests.sh
+++ scripts/updateK8sManifests.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-
 set -x
 
-# Set the repository URL
-REPO_URL="https://6z1at9YLOrwZYfuYfCCmsEIVYhcSuaqBCOsCktEnjk3OuJVonnH8JQQJ99CIACAAAAAAAAAAAAASAZDO4TBc@dev.azure.com/rahulraval/voting-app/_git/voting-app"
+REPO_URL="https://dev.azure.com/rahulraval/voting-app/_git/voting-app"
 
-# Clone the git repository into the /tmp directory
+git config --global http.extraheader "AUTHORIZATION: bearer ${SYSTEM_ACCESSTOKEN}"
+
 git clone "$REPO_URL" /tmp/temp_repo
 
-# Navigate into the cloned repository directory
 cd /tmp/temp_repo
 
-# Make changes to the Kubernetes manifest file(s)
-# For example, let's say you want to change the image tag in a deployment.yaml file
 sed -i "s|image:.*|image: $2/$3:$4|g" k8s-specifications/$1-deployment.yaml
 
 git add .
@@ -24,5 +20,4 @@ git commit -m "Update Kubernetes manifest"
 
 git push
 
-# Cleanup: remove the temporary directory
 rm -rf /tmp/temp_repo
